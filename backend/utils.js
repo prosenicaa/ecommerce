5f411cc1ffec7c3c09211d83974a8455a24d68f7
prosenicaa <andjelaprosenica@gmail.com>
2023-08-31 22:15:22 +0200

    Izmenjen token

--- backend/utils.js
+++ backend/utils.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 import mg from "mailgun-js";
 
+const secretKey = "cva1XeHcW7";
+
 export const generateToken = (user) => {
   return jwt.sign(
     {
@@ -9,7 +11,7 @@ export const generateToken = (user) => {
       email: user.email,
       isAdmin: user.isAdmin,
     },
-    process.env.JWT_SECRET,
+    secretKey,
     {
       expiresIn: "30d",
     }
@@ -20,7 +22,7 @@ export const isAuth = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (authorization) {
     const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
-    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+    jwt.verify(token, secretKey, (err, decode) => {
       if (err) {
         res.status(401).send({ message: "Invalid Token" });
       } else {
